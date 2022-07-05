ad12100c2b986eb3773c08296b7a0ed78b1cdc0d
Lorenn18 <lorenalexandru14@gmail.com>
2022-07-05 16:43:55 +0200

    fin skill page

---
+++ src/components/skillsPage/SkillProgress.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+export default function SkillProgress() {
+  return (
+    <div className="flex space-x-10 pt-16">
+      <div>
+        {/* progress html */}
+
+        <div className="w-[250px] pt-10 ">
+          <div className="flex justify-between ">
+            <p>HTMLS</p>
+            <p>70%</p>
+          </div>
+          <div className="w-full bg-white h-2 rounded-xl">
+            <div className=" h-2 w-[70%] rounded-xl bg-gradient-to-r from-cyan-400 to-blue-700"></div>
+          </div>
+        </div>
+        {/* progress react */}
+
+        <div className="w-[250px] pt-10 ">
+          <div className="flex justify-between ">
+            <p>React</p>
+            <p>100%</p>
+          </div>
+          <div className="w-full bg-white h-2 rounded-xl">
+            <div className=" h-2 w-[100%] rounded-xl bg-gradient-to-r from-green-300 to-orange-500"></div>
+          </div>
+        </div>
+      </div>
+      <div>
+        {/* progress CSS */}
+
+        <div className="w-[250px] pt-10 ">
+          <div className="flex justify-between ">
+            <p>CSS</p>
+            <p>60%</p>
+          </div>
+          <div className="w-full bg-white h-2 rounded-xl">
+            <div className=" h-2 w-[60%] rounded-xl bg-gradient-to-r from-red-600 to-secondary"></div>
+          </div>
+        </div>
+        {/* progress WordPress */}
+
+        <div className="w-[250px] pt-10 ">
+          <div className="flex justify-between ">
+            <p>WordPress</p>
+            <p>90%</p>
+          </div>
+          <div className="w-full bg-white h-2 rounded-xl">
+            <div className=" h-2 w-[70%] rounded-xl bg-gradient-to-r from-lime-400 to-blue-700"></div>
+          </div>
+        </div>
+      </div>
+      <div>
+        {/* progress JS */}
+
+        <div className="w-[250px] pt-10 ">
+          <div className="flex justify-between ">
+            <p>JavaScript</p>
+            <p>75%</p>
+          </div>
+          <div className="w-full bg-white h-2 rounded-xl">
+            <div className=" h-2 w-[75%] rounded-xl bg-gradient-to-r from-blue-800 to-secondary"></div>
+          </div>
+        </div>
+        {/* progress PHP/Laravel */}
+
+        <div className="w-[250px] pt-10 ">
+          <div className="flex justify-between ">
+            <p>PHP/Laravel</p>
+            <p>100%</p>
+          </div>
+          <div className="w-full bg-white h-2 rounded-xl">
+            <div className=" h-2 w-[100%] rounded-xl bg-gradient-to-r from-pink-600 to-orange-500"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/skillsPage/SkillSection.js
+++ src/components/skillsPage/SkillSection.js
@@ -1,11 +1,12 @@
 import React from "react";
+import SkillProgress from "./SkillProgress";
 
 export default function SkillSection() {
   return (
     <div className="pt-14 pl-16">
       <h2 className="text-[#7a798c] leading-3">Ce que je fais bien</h2>
       <h1 className="text-7xl text-[#100f3a] font-black">Mes Compétences</h1>
-      <div className="text-[#7a798c] text-sm pt-60">
+      <div className="text-[#7a798c] text-sm pt-60 w-4/5 ">
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia ducimus
           adipisci inventore asperiores illum iusto nihil ex quaerat cupiditate
@@ -13,10 +14,7 @@ export default function SkillSection() {
           dolorem magnam veniam officiis, iure debitis? Quam.
         </p>
       </div>
-      <div className="w-full bg-gray-200 h-1">
-        <p>HTMLS</p>
-        <div className="bg-blue-600 h-1 w-[70%]"></div>
-      </div>
+      <SkillProgress/>
     </div>
   );
 }
